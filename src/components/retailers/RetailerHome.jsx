977a654519cfd4a697de6a05ceebe920fad75c91
ratandheeraj <ratandheeraj1@gmail.com>
2021-12-03 19:57:29 +0530

    fixed retailer home

--- src/components/retailers/RetailerHome.jsx
+++ src/components/retailers/RetailerHome.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import DistributorCard from "./DistributorCard";
+import Select from 'react-select'
 
 function RetailerHome(){
+    const options = [
+        { value: 'distributor1', label: 'distributor1' },
+        { value: 'distributor2', label: 'distributor2' },
+        { value: 'distributor3', label: 'distributor3' },
+        { value: 'mahesh', label: 'mahesh' },
+        { value: 'manmohan', label: 'manmohan' }
+      ]
+    const addDistributor = (val)=>{
+        alert(val.value);
+    }
     return(
         <div className="flex">
             <div className="w-9/12 ml-5 pl-5">
@@ -11,7 +22,7 @@ function RetailerHome(){
                 <DistributorCard/><DistributorCard/>
             </div>
             <div className="w-2/12 pl-5">
-            <div class="dropdown dropdown-start pt-5">
+            {/* <div class="dropdown dropdown-start pt-5">
                 <div tabindex="0" class="m-1 btn">Add Retailer</div> 
                 <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
                     <li>
@@ -24,9 +35,17 @@ function RetailerHome(){
                     <a>Item 3</a>
                     </li>
                 </ul>
-                </div>
-
+            </div> */}
+            <div class="flex-1 lg:flex-none pt-5 mt-5">
+                {/* <div class="form-control">
+                    <input type="text" placeholder="Search Distributor" class="input"/>
+                </div> */}
+                <label>Add Distributor</label>
+                <Select options={options} className="shadow-md" placeholder="Add Distributor" onChange={addDistributor} />
+            </div> 
             </div>
+           
+            
         </div>
     );
 }
